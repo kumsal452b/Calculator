da8a67679390927dc2159aeeb6878200138feb9b
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-26 17:45:14 +0300

    has finished code element

--- calculateCenter.js
+++ calculateCenter.js
@@ -3,3 +3,31 @@ var num1 = document.getElementById("num1");
 var num2 = document.getElementById("num2");
 
 var selector = document.getElementById("Select1");
+
+var result = 0.0;
+
+var button = document.getElementById("calculate");
+
+function Calculate() {
+    var val1 = num1.value;
+    var val2 = num2.value;
+   
+    if (selector.value = "+") {
+        result = val1 + val2;
+    }
+    else if (selector.value = "-") {
+
+        result = val1 - val2;
+    }
+    else if (selector.value = "*") {
+
+        result = val1 * val2;
+    }
+    else {
+
+        result = val1 / val2;
+    }
+
+    alert(result);
+}
+    button.addEventListener("click", Calculate);
